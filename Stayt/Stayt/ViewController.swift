05e962fee24e5ab1905c273944265399af98fbb8
Владимир Мельников <vlmlnkv@gmail.com>
2018-01-16 21:22:26 +0400

    Initial commit

---
+++ Stayt/Stayt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stayt
+//
+//  Created by Владимир Мельников on 16/01/2018.
+//  Copyright © 2018 Владимир Мельников. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
