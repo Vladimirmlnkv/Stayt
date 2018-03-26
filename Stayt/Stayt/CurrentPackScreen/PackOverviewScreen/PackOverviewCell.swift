130bd4fd87d1449f8ef0ad949376edcdc6bc5d78
Владимир Мельников <vlmlnkv@gmail.com>
2018-03-26 12:23:28 +0400

    Added new pack overview screen

---
+++ Stayt/Stayt/CurrentPackScreen/PackOverviewScreen/PackOverviewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PackOverviewCell.swift
+//  Stayt
+//
+//  Created by Владимир Мельников on 23/03/2018.
+//  Copyright © 2018 Владимир Мельников. All rights reserved.
+//
+
+import UIKit
+
+class PackOverviewCell: UICollectionViewCell {
+    
+    @IBOutlet var dayLabel: UILabel!
+    @IBOutlet var exerciseLabel: UILabel!
+    @IBOutlet var statusImageView: UIImageView!
+    @IBOutlet var startButton: UIButton!
+    @IBOutlet var statusLabel: UILabel!
+    
+    @IBAction func startButtonAction(_ sender: Any) {
+        
+    }
+
+}
---
+++ Stayt/Stayt/CurrentPackScreen/PackOverviewScreen/PackOverviewViewController.swift
@@ -0,0 +1,135 @@
+//
+//  PackOverviewViewController.swift
+//  Stayt
+//
+//  Created by Владимир Мельников on 23/03/2018.
+//  Copyright © 2018 Владимир Мельников. All rights reserved.
+//
+
+import UIKit
+
+class PackOverviewViewController: UIViewController {
+
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var collectionView: UICollectionView!
+    
+    fileprivate var indexOfCellBeforeDragging = 0
+    var exercisePack: ExercisePack!
+    var exerciseDataSource = ExerciseDataSource()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        exercisePack = exerciseDataSource.getBeginnerPack()
+        titleLabel.text = exercisePack.name
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    fileprivate func indexOfMajorCell() -> Int {
+        let itemWidth = cardWidth()
+        let proportionalOffset = collectionView.contentOffset.x / itemWidth
+        return Int(round(proportionalOffset))
+    }
+    
+    fileprivate func cardWidth() -> CGFloat {
+        return collectionView.frame.width - 60 - 10*2
+    }
+}
+
+extension PackOverviewViewController: UICollectionViewDelegateFlowLayout {
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+    }
+
+    func collectionView(_: UICollectionView, layout: UICollectionViewLayout, sizeForItemAt: IndexPath) -> CGSize {
+        let width = cardWidth()
+        let height = collectionView.frame.height - 20
+        return CGSize(width: width, height: height)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+
+}
+
+extension PackOverviewViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return exercisePack.exercises.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let exercise = exercisePack.exercises[indexPath.row]
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PackOverviewCell", for: indexPath) as! PackOverviewCell
+        
+        cell.dayLabel.text = "Day \(indexPath.row + 1)"
+        cell.exerciseLabel.text = exercise.descriptionName
+        
+        cell.startButton.layer.borderColor = Colors.mainActiveColor.cgColor
+        
+        if indexPath.row == exercisePack.currentExerciseNumber {
+            cell.statusLabel.text = "Current day"
+            cell.startButton.setTitle("Start", for: .normal)
+            cell.statusImageView.image = #imageLiteral(resourceName: "circle")
+            cell.startButton.isEnabled = true
+        } else if indexPath.row < exercisePack.currentExerciseNumber {
+            cell.statusLabel.text = "Completed"
+            cell.startButton.setTitle("Repeat", for: .normal)
+            cell.statusImageView.image = #imageLiteral(resourceName: "checkmark")
+            cell.startButton.isEnabled = true
+        } else {
+            cell.statusLabel.text = "Locked"
+            cell.startButton.setTitle("Locked", for: .normal)
+            cell.statusImageView.image = #imageLiteral(resourceName: "lock")
+            cell.startButton.isEnabled = false
+            cell.startButton.layer.borderColor = UIColor.lightGray.cgColor
+        }
+        
+        cell.layer.cornerRadius = 10
+        cell.startButton.layer.borderWidth = 1.0
+        cell.startButton.layer.cornerRadius = 10
+        
+        return cell
+    }
+    
+}
+
+extension PackOverviewViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+    }
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        indexOfCellBeforeDragging = indexOfMajorCell()
+    }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        targetContentOffset.pointee = scrollView.contentOffset
+        let indexOfMajorCell = self.indexOfMajorCell()
+        
+        let swipeVelocityThreshold: CGFloat = 0.5
+        let hasEnoughVelocityToSlideToTheNextCell = indexOfCellBeforeDragging + 1 < exercisePack.exercises.count && velocity.x > swipeVelocityThreshold
+        let hasEnoughVelocityToSlideToThePreviousCell = indexOfCellBeforeDragging - 1 >= 0 && velocity.x < -swipeVelocityThreshold
+        let majorCellIsTheCellBeforeDragging = indexOfMajorCell == indexOfCellBeforeDragging
+        let didUseSwipeToSkipCell = majorCellIsTheCellBeforeDragging && (hasEnoughVelocityToSlideToTheNextCell || hasEnoughVelocityToSlideToThePreviousCell)
+        if didUseSwipeToSkipCell {
+            
+            let freeSpace = (60 + 10 * 2)
+            
+            let snapToIndex = indexOfCellBeforeDragging + (hasEnoughVelocityToSlideToTheNextCell ? 1 : -1)
+            let toValue = cardWidth() * CGFloat(snapToIndex) + 10 + CGFloat(snapToIndex) * 10 - CGFloat(freeSpace / 2)
+            UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: velocity.x, options: .allowUserInteraction, animations: {
+                scrollView.contentOffset = CGPoint(x: toValue, y: 0)
+                scrollView.layoutIfNeeded()
+            }, completion: nil)
+        } else {
+            let indexPath = IndexPath(row: indexOfMajorCell, section: 0)
+            collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+        }
+    }
+    
+}
